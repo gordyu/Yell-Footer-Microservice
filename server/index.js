8f45bb5d6ba388827bb8a951bfd057cdd2ab6145
Gordon Yu <gyunit@gmail.com>
2019-08-26 20:37:25 -0700

    cleanup unnecessary text

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
+
 app.use(express.static(__dirname + '/../client/dist'));
 
 app.listen(port, function() {
